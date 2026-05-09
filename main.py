5dfeb15d6a3691f219ed1a033bd1f4ae02a07de6
Dimitri <dimitriaugusto65@gmail.com>
2026-05-09 16:33:02 -0300

    Correção de erros nas rotas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from database import conectar
 
 app = Flask(__name__)
@@ -8,14 +8,24 @@ def home():
     return "A API está rodando!"
 
 @app.route("/produtos", methods = ["POST"])
-def criar_produto(nome_produto, valor, estoque):
+def criar_produto():
+
+    dados = request.json
+
+    nome_produto = dados["nome"]
+    valor = dados["valor"]
+    estoque = dados["estoque"]
+
     conn, cursor = conectar()
+
     cursor.execute("""
-        INSERT INTO produtos VALUES (nome, valor, estoque), (?, ?, ?)
+        INSERT INTO produtos (nome_produto, valor, estoque) VALUES (?, ?, ?)
 """, (nome_produto, valor, estoque,))
     
     conn.commit()
     conn.close()
+
+    return {"Mensagem": "Produto criado com sucesso!"}
     
 @app.route("/produtos", methods = ["GET"])
 def listar_produtos():
@@ -25,32 +35,38 @@ def listar_produtos():
 """)
     
     dados = cursor.fetchall()
+    produtos = []
 
     for produto in dados:
-        return {"Nome": produto[0],
-                "Valor": produto[1],
-                "Estoque": produto[2]}
+         produtos.append({"Nome": produto[0],
+                        "Valor": produto[1],
+                        "Estoque": produto[2]})
     
     conn.close()
+
+    return jsonify(produtos)
     
 @app.route("/produtos/<int:id>", methods = ["GET"])
 def produto_por_id(id):
     conn, cursor = conectar()
-    cursor.executar("""
+    cursor.execute("""
         SELECT * FROM produtos WHERE id = ?
 """, (id,))
     
     dados = cursor.fetchone()
 
-    for produto in dados:
-        return {"Nome": produto[0],
-                "Valor": produto[1],
-                "Estoque": produto[2]}
+    if not dados:
+        conn.close()
+        return {"Mensagem": "Produto não encontrado!"}
     
     conn.close()
 
+    return {"Nome": dados[0],
+            "Valor": dados[1],
+            "Estoque": dados[2]}
+
 @app.route("/produtos/<int:id>", methods = ["PUT"])
-def atualizar_produto(id, estoque):
+def atualizar_produto(id):
     conn, cursor = conectar()
     dados = request.json
     estoque = dados["estoque"]
@@ -74,9 +90,21 @@ def atualizar_produto(id, estoque):
     return {"Mensagem": "Protudo atualizado com sucesso"}
         
 
-@app.route("/produtos", methods = ["DELETE"])
+@app.route("/produtos/<int:id>", methods = ["DELETE"])
 def deletar_produto(id):
     conn, cursor = conectar()
-    cursor.execute("""
-        DELETE * FROM produtos WHERE id = ?
-""", (id))
+
+    cursor.execute("""SELECT * FROM produtos WHERE id = ?""", (id,))
+    dados = cursor.fetchone()
+
+    if not dados:
+        conn.close()
+        return {"Mensagem": "Produto não encontrado!"}
+    
+    else:
+        cursor.execute("""
+            DELETE FROM produtos WHERE id = ?
+    """, (id,))
+        
+        conn.commit()
+        conn.close()
